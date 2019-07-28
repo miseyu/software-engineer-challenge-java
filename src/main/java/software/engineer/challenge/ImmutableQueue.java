7bcf44c2bbef455d826da387c876357b631d72d7
Yuji Mise <miseyu2000@gmail.com>
2019-07-28 16:11:08 +0900

    modify: immutable queue

---
+++ src/main/java/software/engineer/challenge/ImmutableQueue.java
@@ -0,0 +1,98 @@
+package software.engineer.challenge;
+
+import java.util.NoSuchElementException;
+
+public class ImmutableQueue<T> implements Queue<T> {
+    private static class InnerStack<T> {
+        private T head;
+        private InnerStack<T> tail;
+        private int size;
+
+        private InnerStack() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+
+        private InnerStack(T head, InnerStack<T> tail) {
+            this.head = head;
+            this.tail = tail;
+            this.size = tail.size + 1;
+        }
+
+        private static InnerStack newInstance() {
+            return new InnerStack();
+        }
+
+        public boolean isEmpty() {
+            return this.size == 0;
+        }
+
+        public InnerStack<T> push(T obj) {
+            return new InnerStack(obj, this);
+        }
+
+        public InnerStack toReverseStack() {
+            InnerStack<T> stack = new InnerStack();
+            InnerStack<T> tail = this;
+            while (!tail.isEmpty()) {
+                stack = stack.push(tail.head);
+                tail = tail.tail;
+            }
+            return stack;
+        }
+    }
+
+    private InnerStack<T> order;
+    private InnerStack<T> reverse;
+
+    public ImmutableQueue() {
+        this.order = InnerStack.newInstance();
+        this.reverse = InnerStack.newInstance();
+    }
+
+    public ImmutableQueue(InnerStack<T> order, InnerStack<T> reverse) {
+        this.order = order;
+        this.reverse = reverse;
+    }
+
+    @Override
+    public Queue<T> enQueue(T t) {
+        if (t == null) {
+            throw new IllegalArgumentException();
+        }
+        return new ImmutableQueue(this.order.push(t), this.reverse);
+    }
+
+    @Override
+    public Queue<T> deQueue() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException("queue empty");
+        }
+        if (!this.reverse.isEmpty()) {
+            return new ImmutableQueue(this.order, this.reverse.tail);
+        }
+        return new ImmutableQueue(InnerStack.newInstance(), this.order.toReverseStack().tail);
+    }
+
+    @Override
+    public T head() {
+        if(this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (this.reverse.isEmpty()) {
+            this.reverse= this.order.toReverseStack();
+            this.order = InnerStack.newInstance();
+        }
+        return this.reverse.head;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    protected int size() {
+        return this.order.size + this.reverse.size;
+    }
+}
---
+++ src/test/java/software/engineer/challenge/ImmutableQueueSpecTest.java
@@ -0,0 +1,77 @@
+package software.engineer.challenge;
+
+import static org.junit.Assert.*;
+import static com.greghaskins.spectrum.dsl.specification.Specification.*;
+
+import com.greghaskins.spectrum.Spectrum;
+import org.junit.runner.RunWith;
+
+import java.util.NoSuchElementException;
+
+
+@RunWith(Spectrum.class)
+public class ImmutableQueueSpecTest {
+    {
+        describe("Test of enQueue.", () -> {
+            ImmutableQueue<Integer> queue = new ImmutableQueue<>();
+            it("enQueue 1, 2", () -> {
+                var result = queue.enQueue(1).enQueue(2);
+                assertEquals(result.head(), Integer.valueOf(1));
+            });
+            it("enQueue Null", () -> {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    queue.enQueue(null);
+                });
+            });
+        });
+        describe("Test of deQueue.", () -> {
+            ImmutableQueue<Integer> queue = new ImmutableQueue<>();
+            it("enQueue 1, 2 and deQueue", () -> {
+                var result = queue.enQueue(1).enQueue(2).deQueue();
+                assertEquals(result.head(), Integer.valueOf(2));
+            });
+            it("deQueue in the empty", () -> {
+                assertThrows(NoSuchElementException.class, () -> {
+                    queue.deQueue();
+                });
+            });
+        });
+        describe("Test of head.", () -> {
+            ImmutableQueue<Integer> queue = new ImmutableQueue<>();
+            it("enQueue 3, deQueue, enQueue 1, enQueue 4 and head", () -> {
+                var result = queue.enQueue(3).deQueue().enQueue(1).enQueue(4).head();
+                assertEquals(result, Integer.valueOf(1));
+            });
+            it("head in the empty", () -> {
+                assertThrows(NoSuchElementException.class, () -> {
+                    queue.head();
+                });
+            });
+        });
+        describe("Test of head.", () -> {
+            ImmutableQueue<Integer> queue = new ImmutableQueue<>();
+            it("enQueue 3 and isEmpty", () -> {
+                var result = queue.enQueue(3);
+                assertFalse(result.isEmpty());
+            });
+            it("isEmpty in the empty", () -> {
+                assertTrue(queue.isEmpty());
+            });
+        });
+    }
+    public static void assertThrows(Class<? extends Exception> exceptionClass, Code code) {
+        boolean thrown = false;
+        try {
+            code.run();
+        } catch (Exception ex) {
+            assertTrue(exceptionClass.isInstance(ex));
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @FunctionalInterface
+    public interface Code {
+        public void run() throws Exception;
+    }
+}
